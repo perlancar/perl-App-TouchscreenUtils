29fc6cad1679324db11796ecd78638ce05d43e98
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2019-12-01 18:42:00 +0700

    Initial commit

---
+++ lib/App/BluetoothUtils.pm
@@ -0,0 +1,13 @@
+package App::BluetoothUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLI utilities related to Bluetooth
+
+=head1 DESCRIPTION
+
+This distribution contains the following CLI utilities related to bluetooth:
+
+# INSERT_EXECS_LIST
